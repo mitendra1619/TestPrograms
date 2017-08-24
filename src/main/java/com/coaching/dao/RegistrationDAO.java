12388dd2fd6bd75f50b313d8043a78888d302be8
Manish Sharma <manish.sharma@happiestminds.com>
2017-08-24 13:43:50 +0530

    Added Generics

--- src/main/java/com/coaching/dao/RegistrationDAO.java
+++ src/main/java/com/coaching/dao/RegistrationDAO.java
@@ -5,11 +5,13 @@ import java.util.List;
 import com.coaching.model.StudentEntity;
 import com.coaching.model.SubjectEntity;
 import com.coaching.model.TeacherEntity;
+import com.coaching.travelObject.StudentTO;
+import com.coaching.travelObject.TeacherTO;
 
 public interface RegistrationDAO {
 
-	public List getAllStudents();
-    public List getAllTeachers();
+	public List<StudentTO> getAllStudents();
+    public List<TeacherTO> getAllTeachers();
     public void addStuent(StudentEntity stu);
     public void addTeacher(TeacherEntity tchr);
     public List<SubjectEntity> getAllSubjects();
