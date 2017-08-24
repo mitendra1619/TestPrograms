61e049709abd79b090751ca34135a8a7de45990f
Manish Sharma <manish.sharma@happiestminds.com>
2017-08-24 13:25:48 +0530

    My First commit for Project XXX

--- src/main/java/com/coaching/controller/RegistrationController.java
+++ src/main/java/com/coaching/controller/RegistrationController.java
@@ -3,14 +3,11 @@ package com.coaching.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +22,7 @@ public class RegistrationController {
 	@Autowired
 	RegistrationSvc registrationSvc;
 
+	@SuppressWarnings("unused")
 	private static final Logger logger = LoggerFactory.getLogger(RegistrationController.class);
 	
 	/**
